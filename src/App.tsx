86b7bc9e2d042afe1927a361ed17c07573c10bad
砂町晋矢 <sunamachi.shinya@happylifecreators.com>
2021-11-24 18:55:47 +0900

    個人カレンダー

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
+import './Style.scss'
+// import '@fullcalendar/common/main.css'
+import './TimeLine/fullcalenndar.css'
+
+import { PersonalCalendar } from './TimeLine/PersonalCalendar';
 // import { TestTimeLine2 } from './TestTimeLine2';
 // import { TestTimeLine } from './TestTimeLine';
 // import { TimeLinePage } from './TimeLinePage';
@@ -14,8 +19,9 @@ function App() {
     LONG_TERM : 0,
     MONTH : 1,
     WEEK : 2,
+    PERSONAL : 3,
   };
-  const [type, setType] = useState(TAB_TYPE.LONG_TERM);
+  const [type, setType] = useState(TAB_TYPE.PERSONAL);
   
 
 
@@ -32,6 +38,9 @@ function App() {
         <button type="button"
           onClick={()=>setType(TAB_TYPE.WEEK)}
         >週</button>
+        <button type="button"
+          onClick={()=>setType(TAB_TYPE.PERSONAL)}
+        >個人</button>
       </div>
       {/* <CalendarTimeLine/> */}
       {/* <CustomTimeline /> */}
@@ -47,6 +56,11 @@ function App() {
       }
       {type === TAB_TYPE.WEEK && 
         <TimeLineWeek />
+      }
+      {type === TAB_TYPE.PERSONAL && 
+        <div style={{width:"70%", margin: "1em auto"}}>{/** webの時横幅調整 */}
+            <PersonalCalendar />
+        </div>
       }
     </div>
   );
---
+++ src/TimeLine/PersonalCalendar.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import FullCalendar, {EventInput, EventSourceInput, EventApi, EventSourceApi, DateSelectArg, EventClickArg} from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction"; // 日付クリックイベントに必要
+import allLocales from '@fullcalendar/core/locales-all'; //日本語表記に必要
+import { useCallback, VFC } from "react";
+import '../Style.scss'
+
+
+const INITIAL_EVENTS: EventInput[] = [
+    {
+        id: "1",
+        title: "山陽特殊製鋼姫路",
+        start: "2021-11-01",
+        end: "2021-11-02",// 終了日 + 1日
+        color: '#FEB2B2',
+        textColor: '#63171B',
+        // start: "2021-11-01 11:10",
+        // end: "2021-11-05 11:10",
+        // allDay : false
+    },
+    {
+        id: "2",
+        title: "広島西郵便局",
+        start: "2021-11-01",
+        end: "2021-11-05",
+        color: '#9AE6B4',
+        textColor: '#1C4532',
+    },
+    {
+        id: "3",
+        title: "広島西郵便局",
+        start: "2021-11-07",
+        end: "2021-11-09",
+        color: '#9DECF9',
+        textColor: '#065666',
+    },
+];
+
+export const PersonalCalendar: VFC = () => {
+    const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
+    const handleChangeEvents = useCallback((events: EventApi[]) => {
+      console.log("events:", events);  // 確認用
+        setCurrentEvents(events);
+    }, []);
+
+    /**
+     * 日付クリック
+     */
+    const handleDateClick = useCallback((arg: DateClickArg) => {
+        console.log('handleDateClick');
+        console.log(arg);
+        alert(arg.dateStr);
+    }, []);
+
+    /**
+     * イベント追加日選択してイベント追加
+     */
+    const handleDateSelect = useCallback((selectInfo: DateSelectArg) => {
+        console.log('handleDateSelect');
+        console.log(selectInfo);
+        let title = prompt("イベントのタイトルを入力してください")?.trim();
+        let calendarApi = selectInfo.view.calendar;
+        calendarApi.unselect();
+        if (title) {
+        calendarApi.addEvent({
+            id: "4",
+            title,
+            start: selectInfo.startStr,
+            end: selectInfo.endStr,
+            allDay: selectInfo.allDay,
+        });
+        }
+    }, []);
+
+    /**
+     * イベント選択時（閲覧や編集や削除）
+     */
+    const handleEventClick = useCallback((clickInfo: EventClickArg) => {
+        console.log('handleEventClick');
+        console.log(clickInfo);
+        console.log("events_id:", clickInfo.event._def.publicId);
+        alert(clickInfo.event._def.publicId);
+        // if (window.confirm(`このイベント「${clickInfo.event.title}」を削除しますか`)) {
+        //     clickInfo.event.remove();
+        // }
+    }, []);
+
+    return(
+        // <div style={{backgroundColor:"#FFF"}}>
+            <FullCalendar 
+                locales={allLocales}
+                locale="ja"
+                plugins={[dayGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                initialEvents={INITIAL_EVENTS}
+                eventsSet={handleChangeEvents}//イベントが変わった時
+
+                // dateClick={handleDateClick}
+                // selectable={true}
+                // select={handleDateSelect}
+
+                editable={true}
+                eventClick={handleEventClick}
+                height="auto"
+            />
+        // </div>
+    )
+
+}
